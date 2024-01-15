e5f78335b012dc62a259e2731bb22e7d40852a72
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-01-15 11:07:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/tamil_taj",
+    "https://github.com/mukeshmoni/AmmuMoniMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
